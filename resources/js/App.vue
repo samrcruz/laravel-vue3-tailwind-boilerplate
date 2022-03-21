7d80cdffa9015592d270e893843ec009bdf61c50
samuel cruz <samuelcruz@samuels-MacBook-Pro-3.local>
2022-03-21 16:47:17 +0800

    updates

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="container">
-        <router-view> </router-view>
+<!--    <div class="w-full h-full">-->
+<!--        <router-view> </router-view>-->
+<!--    </div>-->
+    <div class="flex flex-col h-screen justify-between">
+        <header class="h-10 bg-red-500">Header</header>
+        <main class="mb-auto h-10 bg-green-500">
+            <router-view> </router-view>
+        </main>
+        <footer class="h-10 bg-blue-500">Footer</footer>
     </div>
 </template>
 
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,15 +1,39 @@
 <template>
-    <div class="">
-        <h1 class="text-blue-500">laravel + vue + tailwind</h1>
+    <div class="bg-indigo-300">
+        <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+            <h2 class="text-3xl font-extrabold text-gray-500 sm:text-4xl">
+                <span class="block">Laravel + Vue 3 + Tailwind 3</span>
+                <span class="block">Boilerplate</span>
+            </h2>
+            <a href="#" class="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-indigo-50 sm:w-auto"> Login </a>
+        </div>
     </div>
 </template>
 <script>
 
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+import { MenuIcon, XIcon } from '@heroicons/vue/outline'
+
+const navigation = [
+    { name: 'Product', href: '#' },
+    { name: 'Features', href: '#' },
+    { name: 'Marketplace', href: '#' },
+    { name: 'Company', href: '#' },
+]
 
 export default {
     name: "Home",
     components: {
-
+        Popover,
+        PopoverButton,
+        PopoverPanel,
+        MenuIcon,
+        XIcon,
+    },
+    setup() {
+        return {
+            navigation,
+        }
     },
     created() {
 
